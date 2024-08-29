ae57e65e02c80ab3521c0eb2c72ef52606b77c55
phuwadech <aofphuwadech@gmail.com>
2024-08-29 13:43:36 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\lineController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -7,7 +8,9 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('/line/webhook' ,function(){
+Route::post('/line/webhook' ,[lineController::class,'lineWebHook']);
+
+Route::get('/test' ,function(){
     return response()->json([
         'message' => 'test'
     ]);
