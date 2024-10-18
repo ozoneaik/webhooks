cb971cb64128b6c4c5a1fb12f8d843b91e35b1ef
phuwadech <aofphuwadech@gmail.com>
2024-10-18 15:51:50 +0700

    update

--- app/Models/Rates.php
+++ app/Models/Rates.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 /**
  * @method static where(string $string, $custId)
+ * @method static select(string $string)
+ * @method static leftJoin(string $string, string $string1, string $string2, string $string3)
  */
 class Rates extends Model
 {
